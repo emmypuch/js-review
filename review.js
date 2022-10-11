14b81ce8bae21672baf0ca2e043551ac9f6ec025
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2022-10-11 15:57:48 +0100

    cdn vue

---
+++ review.js
@@ -0,0 +1,37 @@
+// const title = "Best reads of 2019";
+// const author = "Mario";
+// const likes = 30;
+
+// // template string method
+// let result = `The blog called ${title} by ${author} has ${likes} likes`;
+// console.log(result);
+
+// Loops
+// for (let i = 0; i < 5; i++) {
+//   console.log(i);
+// }
+
+// const names = ["Ola", "Ade", "Sola"];
+
+// let i = 0;
+// while (i < names.length) {
+//   console.log(names[2]);
+//   i++;
+// }
+
+// let i = 5;
+
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < i.length);
+
+// const age = 25;
+
+// if (age > 20) {
+//   console.log("You are over 20 years old");
+// } else if (age <= 20) {
+//   console.log("You are 20 years old");
+// } else {
+//   console.log("You are less than 20 years old");
+// }
